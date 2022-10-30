1328a7a2ac5116d7e84937edbd873602eca20b85
artemmlnchk <artemmlnchk@ARTEMMELNYC5FB8>
2022-10-30 13:01:55 +0100

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Wpf_Training
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+            public MainWindow()
+            {
+                InitializeComponent();
+                foreach (UIElement el in MainRoot.Children)
+                {
+                    if (el is Button)
+                    {
+                        ((Button)el).Click += Button_Click;
+                    }
+                }
+            }
+
+            private void Button_Click(object sender, RoutedEventArgs e)
+            {
+                string str = (string)((Button)e.OriginalSource).Content;
+                if (str == "C")
+                    TextBox2.Text = "";
+               
+            else if (str == "=")
+                {
+                    string value = new DataTable().Compute(TextBox2.Text, null).ToString();
+                    TextBox1.Text = value;
+                }
+
+            else if(str == "CE")
+            {
+                TextBox1.Text = "";
+            }
+
+            else if (str == "<")
+            {
+
+                string value = new DataTable().Compute(TextBox2.Text, null).ToString();
+               
+                TextBox2.Text = value.Remove(value.Length -1,1);
+               
+
+
+            }
+
+            else
+                TextBox2.Text += str;
+
+            
+
+            
+        }
+        
+    }
+}
